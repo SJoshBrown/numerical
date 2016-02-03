b14cfc5abd579691065a3ba58275189a91edb79a
Josh Brown <S.Josh.Brown@gmail.com>
2016-02-03 13:22:43 -0600

    break splitting off into another function

--- rbmm/rbmm.py
+++ rbmm/rbmm.py
@@ -14,25 +14,29 @@ def validate_matrices(mat_a, mat_b):
     """
     return mat_a.shape[1] == mat_b.shape[0]
 
+def split(mat_a):
+    """
+    Takes one matrix and splits it into four quadrents, returning those
+    four matrices
+    """
+    rows_a = mat_a.shape[0]
+    cols_a = mat_a.shape[1]
+
+    blk_a00 = mat_a[0:rows_a/2, 0:cols_a/2]
+    blk_a01 = mat_a[0:rows_a/2, cols_a/2:cols_a]
+    blk_a10 = mat_a[rows_a/2:rows_a, 0:cols_a/2]
+    blk_a11 = mat_a[rows_a/2:rows_a, cols_a/2:cols_a]
+    return [blk_a00, blk_a01, blk_a10, blk_a11]
+
 
 def block_multiply(mat_a, mat_b):
     """
     Recursively split two matrices until 2x2 or smaller then multiply
     """
     if should_recurse(mat_a, mat_b):
-        rows_a = mat_a.shape[0]
-        cols_a = mat_a.shape[1]
-        rows_b = mat_b.shape[0]
-        cols_b = mat_b.shape[1]
 
-        blk_a00 = mat_a[0:rows_a/2, 0:cols_a/2]
-        blk_a01 = mat_a[0:rows_a/2, cols_a/2:cols_a]
-        blk_a10 = mat_a[rows_a/2:rows_a, 0:cols_a/2]
-        blk_a11 = mat_a[rows_a/2:rows_a, cols_a/2:cols_a]
-        blk_b00 = mat_b[0:rows_b/2, 0:cols_b/2]
-        blk_b01 = mat_b[0:rows_b/2, cols_b/2:cols_b]
-        blk_b10 = mat_b[rows_b/2:rows_b, 0:cols_b/2]
-        blk_b11 = mat_b[rows_b/2:rows_b, cols_b/2:cols_b]
+        blk_a00, blk_a01, blk_a10, blk_a11 = split(mat_a)
+        blk_b00, blk_b01, blk_b10, blk_b11 = split(mat_b)
 
         blk_a = block_multiply(blk_a00, blk_b00) + \
                 block_multiply(blk_a01, blk_b10)
@@ -42,9 +46,9 @@ def block_multiply(mat_a, mat_b):
                 block_multiply(blk_a11, blk_b10)
         blk_d = block_multiply(blk_a10, blk_b01) + \
                 block_multiply(blk_a11, blk_b11)
-        blk_ab = NP.concatenate((blk_a, blk_b), axis=1)
-        blk_cd = NP.concatenate((blk_c, blk_d), axis=1)
-        return NP.concatenate((blk_ab, blk_cd), axis=0)
+
+        return NP.concatenate((NP.concatenate((blk_a, blk_b), 1),\
+                               NP.concatenate((blk_c, blk_d), 1)), 0)
 
     else:
         return mat_a * mat_b
@@ -74,6 +78,7 @@ if validate_matrices(A, B):
     print R
     print NP.testing.assert_array_almost_equal(R, T)
     NP.savetxt(argv[3], R)
+    NP.savetxt('default_method.txt', T)
 
 else:
     print ("Matrix dimension Error - Cannot take the product of a %dx%d and a "
