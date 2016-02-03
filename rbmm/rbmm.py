06e4d4c9ae1c7a6e58c54f1032801c22745dfca9
Josh Brown <S.Josh.Brown@gmail.com>
2016-02-03 11:20:43 -0600

    more refactoring...

--- rbmm/rbmm.py
+++ rbmm/rbmm.py
@@ -2,24 +2,24 @@
 RBMM - Recursive block matrix multipication.
 By Josh Brown
 CS 3513 - Numerical methods
-
+Due - 2/10/16
 """
 import numpy
 from sys import argv
 
 
-def validateMatricesForMultiplication(matA, matB):
+def validate_matrices(matA, matB):
     """
     return true if matrices are well conditioned for multipication
     """
     return matA.shape[1] == matB.shape[0]
 
 
-def recursiveBlockMultiply(matA, matB):
+def block_multiply(matA, matB):
     """
     Recursively split two matrices until 2x2 or smaller then multiply
     """
-    if(shouldRecurse(matA,matB)):
+    if(should_recurse(matA, matB)):
         rowsA = matA.shape[0]
         colsA = matA.shape[1]
         rowsB = matB.shape[0]
@@ -34,19 +34,19 @@ def recursiveBlockMultiply(matA, matB):
         B10 = matB[rowsB/2:rowsB,0:colsB/2]
         B11 = matB[rowsB/2:rowsB,colsB/2:colsB]
 
-        A = recursiveBlockMultiply(A00,B00) + recursiveBlockMultiply(A01, B10)
-        B = recursiveBlockMultiply(A00,B01) + recursiveBlockMultiply(A01, B11)
-        C = recursiveBlockMultiply(A10,B00) + recursiveBlockMultiply(A11, B10)
-        D = recursiveBlockMultiply(A10,B01) + recursiveBlockMultiply(A11, B11)
-        AB = NP.concatenate((A,B), axis=1)
-        CD = NP.concatenate((C,D), axis=1)
-        return NP.concatenate((AB,CD), axis=0)
+        A = block_multiply(A00, B00) + block_multiply(A01, B10)
+        B = block_multiply(A00, B01) + block_multiply(A01, B11)
+        C = block_multiply(A10, B00) + block_multiply(A11, B10)
+        D = block_multiply(A10, B01) + block_multiply(A11, B11)
+        AB = NP.concatenate((A, B), axis=1)
+        CD = NP.concatenate((C, D), axis=1)
+        return NP.concatenate((AB, CD), axis=0)
 
     else:
         return matA * matB
 
 
-def shouldRecurse(matA, matB):
+def should_recurse(matA, matB):
     """
     Return true if matA and matB both have dimensions greater than 2x2
     """
@@ -64,13 +64,13 @@ B = NP.loadtxt(argv[2])
 A = NP.matrix(A)
 B = NP.matrix(B)
 
-if validateMatricesForMultiplication(A, B):
-    R = recursiveBlockMultiply(A,B)
+if validate_matrices(A, B):
+    R = block_multiply(A, B)
     T = A*B
     print R
     print NP.testing.assert_array_almost_equal(R, T)
-    NP.savetxt(argv[3],R)
+    NP.savetxt(argv[3], R)
 
 else:
     print ("Matrix dimension Error - Cannot take the product of a %dx%d and a "
-    "%dx%d matrix.") % (A.shape[0],A.shape[1],B.shape[0],B.shape[1])
+    "%dx%d matrix.") % (A.shape[0], A.shape[1], B.shape[0], B.shape[1])
